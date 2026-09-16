51c808cc1328e90b8d487f18f5451d0b7cfbf2bc
zelospersonalapp-cpu <zelospersonalapp@gmail.com>
2026-09-16 16:04:44 -0300

    copy: menu Análise de Conduta

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -33,7 +33,7 @@ export const Layout: React.FC<LayoutProps> = ({ children, onOpenNovoAgendamento 
     { name: 'Agenda (Calendário)', href: '/agenda', icon: Calendar },
     { name: 'PTAs (Plataformas)', href: '/ptas', icon: Truck },
     { name: 'Avarias & Manutenção', href: '/avarias', icon: AlertOctagon },
-    { name: 'Desconformidades', href: '/desconformidades', icon: TrendingDown },
+    { name: 'Análise de Conduta', href: '/desconformidades', icon: TrendingDown },
     { name: 'Recorrências', href: '/recorrencias', icon: Repeat },
     { name: 'Cadastros Auxiliares', href: '/cadastros', icon: Database },
   ];
